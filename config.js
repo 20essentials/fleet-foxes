fdebed88ad7f3266957b2c3f82b8114d4e989b45
DominicDev <s0l0estudi4r@gmail.com>
2025-09-10 17:49:53 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'FLEET FOXES';
+export const nameSongs = [
+	"FOOL'S ERRAND", /*1*/
+	"THIRD OF MAY", /*2*/
+	"CAN I BELIEVE YOU", /*3*/
+	"IF YOU NEED TO, KEEP TIME ON ME", /*4*/
+	"I AM ALL THAT I NEED", /*5*/
+	"SUNBLIND", /*6*/
+	"ICICLE TUSK", /*7*/
+	"ON ANOTHER OCEAN", /*8*/
+	"NAIADS", /*9*/
+	"FEATHERWEIGHT", /*10*/
+	"CASSIUS", /*11*/
+	"KEPT WOMAN", /*12*/
+	"CRACK-UP", /*13*/
+	"JARA", /*14*/
+	"A LONG WAY PAST THE PAST", /*15*/
+	"GOING-TO-THE-SUN ROAD", /*16*/
+	"MAESTRANZA", /*17*/
+	"I'M NOT MY SEASON", /*18*/
+	"I SHOULD SEE MEMPHIS", /*19*/
+	"MEARCSTAPA", /*20*/
+]
